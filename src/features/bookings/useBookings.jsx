ce32f180f0225cd1ff0f0e1cb0509dc10d6d9d51
Eng-Nafad <nafadayman@yahoo.com>
2026-07-06 23:20:21 +0300

    Fix import case for GitHub Actions

--- src/features/bookings/useBookings.jsx
+++ src/features/bookings/useBookings.jsx
@@ -1,5 +1,5 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { getBookings } from "../../services/apiBookings";
+import { getBooking } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 import { PAGE_SIZE } from "../../utils/constants";
 
